5cadf9cfbbd0362e8e847b57822866b3498f3bfb
Jay Flowers <jay.flowers@gmail.com>
2017-11-14 10:57:44 -0500

    initial attempt

---
+++ attributes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: atom-organism
+# Attributes:: default
+#
+
+default[:atom_organism][:packages] = %w{
+  language-chef
+  language-groovy
+  markdown-writer
+  path-copy
+  Sublime-Style-Column-Selection
+  aligner
+  atom-beautify
+  change-case
+  cucumber
+  git-plus
+  git-time-machine
+  highlight-selected
+  minimap
+  minimap-highlight-selected
+  open-recent
+  todo-show
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,30 @@
 # Cookbook Name:: atom-organism
 # Recipe:: default
 #
+
+require 'pathname'
+
+include_recipe 'atom'
+
+node[:atom_organism][:packages].each{|atom_package|
+  atom_apm atom_package
+}
+
+organism_directory = File.expand_path('../files/default/organism', File.dirname(__FILE__))
+
+Dir.glob("#{organism_directory}/**/*"){|io_object|
+  relative_path_io_object = Pathname.new(io_object).relative_path_from(Pathname.new(organism_directory)).to_s
+  target_path = "#{$WORKSPACE_SETTINGS[:paths][:project][:workspace][:settings][:organisms][:home]}/atom/#{relative_path_io_object}"
+
+  if File.directory?(io_object)
+    directory target_path do
+      recursive true
+      owner ENV['USER']
+    end
+  else
+    cookbook_file target_path do
+      source relative_path_io_object
+      owner ENV['USER']
+    end
+  end
+}
